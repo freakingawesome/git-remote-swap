a94586c954f1b837db35ec71685159222ba59244
Chad Gilbert <chad.gilbert@cqlcorp.com>
2021-02-02 07:24:32 -0500

    Wrap in vscode dev container

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+/*--------------------------------------------------------------------------------------------------------------
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License. See https://go.microsoft.com/fwlink/?linkid=2090316 for license information.
+ *-------------------------------------------------------------------------------------------------------------*/
+
 fn main() {
-    println!("Hello, world!");
+    let name = "VS Code Remote - Containers";
+    println!("Hello, {}!", name);
 }
+
