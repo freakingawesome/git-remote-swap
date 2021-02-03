80b3e7010f5ed85bc0367a8287939c0781c38097
Chad Gilbert <chad.gilbert@cqlcorp.com>
2021-02-03 03:02:20 -0500

    Format on save

--- src/lib/lib.rs
+++ src/lib/lib.rs
@@ -1,13 +1,12 @@
-use std::{path, io};
-use walkdir::{WalkDir};
 use git2::Repository;
+use std::path;
+use walkdir::WalkDir;
 
 pub fn is_likely_git_repo(dir: &path::PathBuf) -> bool {
-    dir.ends_with(".git")
-        || (dir.join("refs").is_dir() && dir.join("config").is_file())
+    dir.ends_with(".git") || (dir.join("refs").is_dir() && dir.join("config").is_file())
 }
 
-pub fn visit_git_repos(root: &path::PathBuf) -> impl Iterator<Item=Repository> {
+pub fn visit_git_repos(root: &path::PathBuf) -> impl Iterator<Item = Repository> {
     WalkDir::new(root)
         .follow_links(true)
         .into_iter()
@@ -20,24 +19,23 @@ pub fn visit_git_repos(root: &path::PathBuf) -> impl Iterator<Item=Repository> {
 
 #[allow(dead_code)]
 mod tests {
-    use std::collections::HashSet;
-    use git2::Repository;
     use super::*;
-
-    use tempfile::Builder;
+    use git2::Repository;
+    use std::collections::HashSet;
     use std::{fs, io, path};
-    
+    use tempfile::Builder;
+
     enum RepoType {
         None,
         WorkDir,
         Bare,
     }
-    
+
     #[test]
     fn test_is_likely_git_repo_none() {
         test_is_likely_git_repo(RepoType::None, false)
     }
-    
+
     #[test]
     fn test_is_likely_git_repo_workdir() {
         with_temp_dir(|temp_path| {
@@ -45,53 +43,94 @@ mod tests {
             make_git_repo(RepoType::WorkDir, &dir)?;
             assert_eq!(is_likely_git_repo(&dir.join(".git")), true);
             Ok(())
-        }).unwrap();
+        })
+        .unwrap();
     }
-    
+
     #[test]
     fn test_is_likely_git_repo_bare() {
         test_is_likely_git_repo(RepoType::Bare, true)
     }
-    
+
     #[test]
     fn test_visit_sub_repos() {
         with_temp_dir(|temp_path| {
             make_git_repo(RepoType::None, &temp_path.join("not_a_repo"))?;
             make_git_repo(RepoType::WorkDir, &temp_path.join("a_repo"))?;
             make_git_repo(RepoType::Bare, &temp_path.join("a_bare_repo"))?;
-            make_git_repo(RepoType::WorkDir, &temp_path.join("a").join("deeply").join("nested").join("repo"))?;
-            make_git_repo(RepoType::Bare, &temp_path.join("another").join("deeply").join("nested").join("bare_repo"))?;
+            make_git_repo(
+                RepoType::WorkDir,
+                &temp_path
+                    .join("a")
+                    .join("deeply")
+                    .join("nested")
+                    .join("repo"),
+            )?;
+            make_git_repo(
+                RepoType::Bare,
+                &temp_path
+                    .join("another")
+                    .join("deeply")
+                    .join("nested")
+                    .join("bare_repo"),
+            )?;
 
-            let visited: HashSet<path::PathBuf> = visit_git_repos(&temp_path).map(|r| r.path().to_path_buf()).collect();
-            
+            let visited: HashSet<path::PathBuf> = visit_git_repos(&temp_path)
+                .map(|r| r.path().to_path_buf())
+                .collect();
             let assert_path_visited = |p: &path::PathBuf| assert_eq!(visited.contains(p), true);
 
             assert_path_visited(&temp_path.join("a_repo").join(".git"));
             assert_path_visited(&temp_path.join("a_bare_repo"));
-            assert_path_visited(&temp_path.join("a").join("deeply").join("nested").join("repo").join(".git"));
-            assert_path_visited(&temp_path.join("another").join("deeply").join("nested").join("bare_repo"));
+            assert_path_visited(
+                &temp_path
+                    .join("a")
+                    .join("deeply")
+                    .join("nested")
+                    .join("repo")
+                    .join(".git"),
+            );
+            assert_path_visited(
+                &temp_path
+                    .join("another")
+                    .join("deeply")
+                    .join("nested")
+                    .join("bare_repo"),
+            );
 
-            assert_eq!(visited.len(), 4, "There exist more folders interpreted as repos than expected");
+            assert_eq!(
+                visited.len(),
+                4,
+                "There exist more folders interpreted as repos than expected"
+            );
 
             Ok(())
-        }).unwrap();
+        })
+        .unwrap();
     }
-    
+
     fn with_temp_dir<T>(run_test: T) -> io::Result<()>
-        where T: FnOnce(path::PathBuf) -> io::Result<()>
+    where
+        T: FnOnce(path::PathBuf) -> io::Result<()>,
     {
-        let tmp_dir = Builder::new().prefix("git-remote-swap-testrun-").tempdir()?;
+        let tmp_dir = Builder::new()
+            .prefix("git-remote-swap-testrun-")
+            .tempdir()?;
         run_test(tmp_dir.path().to_path_buf())?;
         tmp_dir.close()
     }
-    
     fn make_git_repo(which: RepoType, at: &path::PathBuf) -> io::Result<()> {
         fs::create_dir_all(at)?;
-        
         match which {
-            RepoType::None => {();},
-            RepoType::WorkDir => {Repository::init(at).unwrap();},
-            RepoType::Bare => {Repository::init_bare(at).unwrap();},
+            RepoType::None => {
+                ();
+            }
+            RepoType::WorkDir => {
+                Repository::init(at).unwrap();
+            }
+            RepoType::Bare => {
+                Repository::init_bare(at).unwrap();
+            }
         };
 
         Ok(())
@@ -103,6 +142,7 @@ mod tests {
             make_git_repo(repo_type, &dir)?;
             assert_eq!(is_likely_git_repo(&dir), expected);
             Ok(())
-        }).unwrap();
+        })
+        .unwrap();
     }
 }
